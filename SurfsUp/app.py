b44d52ad50b233a04f2469343f0e780c0b5cef77
Taryn Fordyce <fordycetaryn@gmail.com>
2023-08-10 21:31:17 +1000

    cleaned up final code

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -63,7 +63,7 @@ def precipitation():
     start_date = '2016-08-23'
     sel = [measurement.date,
        func.sum(measurement.prcp)]
-    precipitation_data = session.query(*sel).\
+    precipitation = session.query(*sel).\
     filter(measurement.date >= start_date).\
     group_by(measurement.date).\
     order_by(measurement.date).all()
@@ -84,10 +84,10 @@ def precipitation():
 
 # Do the same for stations
 
-    @app.route("/api/v1.0/stations")
-    def stations():
-        session = Session(engine)
-        sel = [measurement.station]
+@app.route("/api/v1.0/stations")
+def stations():
+    session = Session(engine)
+    sel = [measurement.station]
     active_stations = session.query(*sel).\
     group_by(measurement.station).all()
 
@@ -99,22 +99,77 @@ def precipitation():
 
 @app.route("/api/v1.0/tobs")
 def tobs():
-     session = Session(engine)
+    session = Session(engine)
 # Query the dates and temperature observations of the most-active station for the previous year of data.
-     start_date = '2016-08-23'
-     sel = [measurement.date,
-            measurement.tobs]
-station_temps = session.query(*sel).\
+    start_date = '2016-08-23'
+    sel = [measurement.date,
+       measurement.tobs]
+    station_temps = session.query(*sel).\
         filter(measurement.date >= start_date, measurement.station == 'USC00519281').\
         group_by(measurement.date).\
         order_by(measurement.date).all()
      
-session.close()
+    session.close()
 
 # Create a dictionary with the date as the key the temperature as the value
-observation_dates = []
-temp_observations = []
+    observation_dates = []
+    temp_observations = []
 
-for date, observation in station_temps:
-     observation_dates.append(date)
-     temp_observations.append(observation)
+    for date, observation in station_temps:
+        observation_dates.append(date)
+        temp_observations.append(observation)
+
+    most_active_tobs_dict = dict(zip(observation_dates, temp_observations))
+# Return a JSON list of the minimum temperature, the average temperature, 
+# and the maximum temperature for a specified start or start-end range.
+    return jsonify(most_active_tobs_dict)
+
+@app.route("/api/v1.0/trip/<start_date>")
+def trip1(start_date, end_date='2017-08-23'):
+    session = Session(engine)
+    query_result = session.query(func.min(measurement.tobs), func.avg(measurement.tobs), func.max(measurement.tobs)).\
+        filter(measurement.date >= start_date).filter(measurement.date <= end_date).all()
+    session.close()
+# For a specified start, calculate TMIN, TAVG, and TMAX for all the dates greater than or equal to the start date.
+     
+    trip_stats = []
+    for min, avg, max in query_result:
+        trip_dict = {}
+        trip_dict["Min"] = min
+        trip_dict["Average"] = avg
+        trip_dict["Max"] = max
+        trip_stats.append(trip_dict)
+
+    # If the query returns non-null values return the results,
+    # otherwise return an error message
+    if trip_dict['Min']: 
+        return jsonify(trip_stats)
+    else:
+        return jsonify({"error": f"Date {start_date} not found or not formatted as YYYY-MM-DD."}), 404
+    
+
+@app.route("/api/v1.0/trip/<start_date>/<end_date>")
+def trip2(start_date, end_date='2017-08-23'):
+
+# For a specified start date and end date, calculate TMIN, TAVG, and TMAX for the dates from the start date to the end date, inclusive.
+
+    session = Session(engine)
+    query_result = session.query(func.min(measurement.tobs), func.avg(measurement.tobs), func.max(measurement.tobs)).\
+        filter(measurement.date >= start_date).filter(measurement.date <= end_date).all()
+    session.close()
+
+    trip_stats = []
+    for min, avg, max in query_result:
+        trip_dict = {}
+        trip_dict["Min"] = min
+        trip_dict["Average"] = avg
+        trip_dict["Max"] = max
+        trip_stats.append(trip_dict)
+
+    # If the query returns non-null values return the results,
+    # otherwise return an error message
+
+    if trip_dict['Min']: 
+        return jsonify(trip_stats)
+    else:
+        return jsonify({"error": f"Date(s) not found, invalid date range or dates not formatted correctly."}), 404
